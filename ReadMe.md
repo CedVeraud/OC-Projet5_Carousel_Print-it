4894fe8e78c53d23886eed2b7620cf246763e243
Cedric Veraud <pegeco@gmail.com>
2025-02-12 11:24:27 +0100

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,24 @@
+# Print-It Slider
+## Projet N°5 - Formation intégrateur web d'OPENCLASSROOMS
+## Auteur - Cédric Véraud
+## Février 2025
+
+## Synopsis
+Je décroche ma première mission freelance.
+Il s'agit de dynamiser, via l'ajout d'un carousel d'images, l'accueil du site Print-it, TPE familiale gérée par Lucien Gerodo, mon interlocuteur pour cette mission.
+
+## Objectif
+- Intégrer à l'accueil du site un carousel dynamique infini en javascript
+  
+## Éléments fournis 
+- Le CSS est déjà en place
+- Les images à afficher se trouvent dans le dossier ./assets/images/slideshow
+- Les flèches left/right sont fournies dans ./assets/images/
+- Le tableau javascript des différents slides est déjà construit
+
+## Règles
+- L'image et la TagLine changent au click sur les fleches
+- les dots sont créés dynamiquement selon le nombre d'images
+- (option personnelle : les dots sont cliquables et permettent également de naviguer dans le slideshow)
+- Le slideshow boucle sur un retour à la première image
+
---
+++ assets/script.js
@@ -0,0 +1,43 @@
+const slides = [
+	{
+		"image":"slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image":"slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image":"slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image":"slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+]
+
+// VARIABLES
+const bannerContainer = document.getElementById("banner");
+const prevArrow = document.querySelector(".arrow_left");
+const nextArrow = document.querySelector(".arrow_right");
+
+// Diapositive précédente
+function prevSlide() {
+	console.log("prev")
+}
+
+// Diapositive suivante
+function nextSlide() {
+	console.log("next")
+}
+
+// clicks sur les flèches gauche et droite
+prevArrow.addEventListener("click", prevSlide);
+nextArrow.addEventListener("click", nextSlide);
+
+// Restrictions du click droit
+bannerContainer.addEventListener("contextmenu", (event) => {
+    console.log("right click is not allowed here")
+	event.preventDefault()
+})
